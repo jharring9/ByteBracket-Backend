dda971ac6a3ceff938a7d61b824ef81e081f2f3e
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-03-09 17:02:00 -0600

    New config

--- src/redisClient.js
+++ src/redisClient.js
@@ -2,7 +2,7 @@
 const Redis = require("ioredis");
 
 exports.redisClient = new Redis({
-  host: "bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com",
+  host: "bytebracket-redis-001.3mcqkz.0001.use1.cache.amazonaws.com",
   port: 6379,
   tls: {
     rejectUnauthorized: false,
