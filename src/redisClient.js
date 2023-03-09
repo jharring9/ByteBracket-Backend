1556acd0df8201006ceb7b09143ae33e4996d70c
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-03-09 16:42:05 -0600

    Add IORedis

--- src/redisClient.js
+++ src/redisClient.js
@@ -2,7 +2,7 @@
 const Redis = require("ioredis");
 
 exports.redisClient = new Redis({
-  host: "bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com",
+  host: "redis://bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com",
   port: 6379,
   tls: {},
   family: 6,
