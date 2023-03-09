c64f833360c2a4b97a83d956dc9f77b649349130
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-03-09 16:58:47 -0600

    Update redis

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,7 +1,9 @@
 /* Setup Redis client */
 const Redis = require("ioredis");
 
-exports.redisClient = new Redis({
-  host: "bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com",
-  port: 6379,
-});
+exports.redisClient = new Redis(
+  "redis://bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com:6379",
+  {
+    tls: {},
+  }
+);
