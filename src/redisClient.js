55fe66e6aec1f4991dc20c1ce72b27890ab6f1dc
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-03-09 16:46:10 -0600

    Get rid of IORedis

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,7 +1,11 @@
 /* Setup Redis client */
-const Redis = require("ioredis");
+const RedisClustr = require("redis-clustr");
 
-exports.redisClient = new Redis({
-  host: "bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com",
-  tls: {},
+exports.redisClient = new RedisClustr({
+  servers: [
+    {
+      host: "bytebracket-redis.3mcqkz.ng.0001.use1.cache.amazonaws.com",
+      port: 6379,
+    },
+  ],
 });
