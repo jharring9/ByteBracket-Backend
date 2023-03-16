33ce951f9d3cddebb0af0df6b8a80012c674c583
Jack Harrington <47326372+jharring9@users.noreply.github.com>
2023-03-16 17:35:53 -0500

    Fix redis json format

--- src/dynamo/bracket.cjs
+++ src/dynamo/bracket.cjs
@@ -90,27 +90,36 @@ exports.deleteBracket = async (username, id) => {
 
 exports.batchGetBrackets = async (entries) => {
   if (entries.length === 0) return [];
-  console.log(entries);
-  const brackets = entries.filter((e, i) => i % 2 === 0);
-  const points = entries.filter((e, i) => i % 2 === 1);
+
+  // Create a map of entry ID to points
+  const pointsMap = {};
+  for (let i = 0; i < entries.length; i += 2) {
+    const data = JSON.parse(entries[i]);
+    pointsMap[data.bracket] = {
+      points: entries[i + 1],
+      user: data.user,
+    };
+  }
+
   const params = {
     RequestItems: {
       [bracketTable]: {
-        Keys: brackets.map((entry) => {
-          const data = JSON.parse(entry);
+        Keys: Object.keys(pointsMap).map((bracketId) => {
           return {
-            username: data.user,
-            id: data.bracket,
+            username: pointsMap[bracketId].user,
+            id: bracketId,
           };
         }),
         AttributesToGet: ["id", "username", "name", "winnerName"],
       },
     },
   };
+  console.log(params);// TODO -- remove
   try {
     const { Responses } = await ddbDocClient.send(new BatchGetCommand(params));
-    return Responses[bracketTable].map((bracket, index) => {
-      bracket.points = points[index];
+    console.log(Responses[bracketTable]); // TODO -- remove
+    return Responses[bracketTable].map((bracket) => {
+      bracket.points = pointsMap[bracket.id].points;
       return bracket;
     });
   } catch (err) {
